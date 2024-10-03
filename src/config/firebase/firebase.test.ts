13a273d015fa5762a23e624ab1455fc090b8c704
vkotliarenko <vlad.kotlyarenko0@gmail.com>
2024-10-03 21:38:14 +0300

    GRX-45. Add firebase config

---
+++ src/config/firebase/firebase.test.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+import { firebaseApp } from './index';
+
+jest.mock('firebase/app', () => ({
+  initializeApp: jest.fn(),
+}));
+
+jest.mock('firebase/analytics', () => ({
+  getAnalytics: jest.fn(),
+}));
+
+describe('Firebase Initialization', () => {
+  it('should initialize Firebase app with the correct config', () => {
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    };
+
+    expect(initializeApp).toHaveBeenCalledWith(firebaseConfig);
+  });
+
+  it('should initialize Firebase Analytics', () => {
+    expect(getAnalytics).toHaveBeenCalledWith(firebaseApp);
+  });
+});
