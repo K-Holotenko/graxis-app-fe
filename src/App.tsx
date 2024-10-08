0f0a414958a88ae705c18850229c4a3fbe48cfd8
Kyrylo Holotenko <kirya.golotenko@gmail.com>
2024-10-09 02:49:24 +0300

    NONE Fixed linter issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <div className="App">Graxis</div>;
-};
+const App: React.FC = () => <div className="App">Graxis</div>;
 
 export default App;
