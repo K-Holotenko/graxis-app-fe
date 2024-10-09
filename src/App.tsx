9298d5b8d102882fbcab46ab721702da9eca1a9a
vkotliarenko <vlad.kotlyarenko0@gmail.com>
2024-10-09 17:15:36 +0300

    Hotfix. Test App.tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const App: React.FC = () => <div className="App">Graxis</div>;
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Test page</h1>
+      <hr />
+      <h2>Graxis</h2>
+    </div>
+  );
+};
 
 export default App;
