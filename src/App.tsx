0305602aa629bcc997fedf103b52e21ff3f6f28d
Kyrylo Holotenko <kirya.golotenko@gmail.com>
2024-10-13 02:43:21 +0300

    NONE Update linting configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import { Index } from './pages';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Test page</h1>
-      <hr />
-      <h2>Graxis</h2>
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div className="App">
+    <h1>Test page</h1>
+    <hr />
+    <h2>Graxis</h2>
+    <Index />
+  </div>
+);
 
 export default App;
