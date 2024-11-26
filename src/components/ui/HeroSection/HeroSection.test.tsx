0b2f1d18e7ecaf570dd55809821751d54b0ece2a
Alina Haisiuk <alina.gaysyuk@gmail.com>
2024-11-27 01:57:46 +0200

    GRX-87. Fix PR comments

--- src/components/ui/HeroSection/HeroSection.test.tsx
+++ src/components/ui/HeroSection/HeroSection.test.tsx
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { HeroSection } from './index';
 
-describe('HeroSection Component', () => {
-  it('Renders the Hero section with all elements', () => {
+describe('HeroSection', () => {
+  it('should include all elements in the Hero section', () => {
     const { getByText, getByPlaceholderText } = render(<HeroSection />);
 
     expect(getByText('ТВОЯ ПЛАТФОРМА ДЛЯ ШЕРІНГУ')).toBeInTheDocument();
@@ -17,7 +17,7 @@ describe('HeroSection Component', () => {
     expect(input).toBeInTheDocument();
   });
 
-  it('Renders input field and allows text entry in Hero section', async () => {
+  it('should have input field and allows text entry in Hero section', async () => {
     const { getByPlaceholderText } = render(<HeroSection />);
 
     const input = getByPlaceholderText('Пошук товару');
@@ -26,20 +26,5 @@ describe('HeroSection Component', () => {
 
     await userEvent.type(input, 'Книга');
     expect(input).toHaveValue('Книга');
-  });
-
-  it('Hero section should have a background image', () => {
-    const { container } = render(<HeroSection />);
-
-    const heroSectionContainer = container.querySelector(
-      '.hero-section-container'
-    );
-
-    if (heroSectionContainer) {
-      const backgroundImage =
-        getComputedStyle(heroSectionContainer).backgroundImage;
-
-      expect(backgroundImage).toMatch(/url\(/);
-    }
   });
 });
--- src/components/ui/HeroSection/index.tsx
+++ src/components/ui/HeroSection/index.tsx
@@ -3,15 +3,12 @@ import styles from './styles.module.scss';
 import { TEXT } from 'config/constants';
 
 export const HeroSection = () => (
-  <div className={styles['hero-section-container']}>
-    <div className={styles['hero-section']}>
-      <div className={styles['hero-content']}>
+  <div className={styles.heroSectionContainer}>
+    <div className={styles.heroSection}>
+      <div className={styles.heroContent}>
         <h1>{TEXT.HERO_SECTION_TITLE}</h1>
         <h2>{TEXT.HERO_SECTION_SUBTITLE}</h2>
-        <Input
-          className={styles['hero-section-input']}
-          placeholder="Пошук товару"
-        />
+        <Input className={styles.heroSectionInput} placeholder="Пошук товару" />
       </div>
     </div>
   </div>
