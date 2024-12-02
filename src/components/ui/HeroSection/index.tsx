9fc36f87b1d93c0ac3b8a8c343e76437932bdba3
Alina Haisiuk <alina.gaysyuk@gmail.com>
2024-12-03 01:37:42 +0200

    Fix bugs

--- src/components/ui/HeroSection/index.tsx
+++ src/components/ui/HeroSection/index.tsx
@@ -1,4 +1,5 @@
 import { Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 import { TEXT } from 'src/config/constants';
 
@@ -10,7 +11,12 @@ export const HeroSection = () => (
       <div className={styles.heroContent}>
         <h1>{TEXT.HERO_SECTION_TITLE}</h1>
         <h2>{TEXT.HERO_SECTION_SUBTITLE}</h2>
-        <Input className={styles.heroSectionInput} placeholder="Пошук товару" />
+        <Input
+          className={styles.heroSectionInput}
+          placeholder="Пошук товару"
+          prefix={<SearchOutlined />}
+          style={{ borderRadius: '10px' }}
+        />
       </div>
     </div>
   </div>
