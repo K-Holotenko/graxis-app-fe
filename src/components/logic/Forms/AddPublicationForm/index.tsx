e4e0475da0ff34e62e901c48bf101c24b9b429fd
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2024-12-23 15:08:03 +0200

    feat(add publ. price): add required rule, responsive layout

--- src/components/logic/Forms/AddPublicationForm/index.tsx
+++ src/components/logic/Forms/AddPublicationForm/index.tsx
@@ -1,11 +1,16 @@
+import './styles.scss';
 import { Form, Input, ConfigProvider, Col, Row } from 'antd';
+import { useState } from 'react';
 
 import { FORMS, TEXT } from 'src/config/constants';
 import { ReactComponent as HryvniaIcon } from 'src/assets/icons/hryvnia.svg';
 import { TextAreaFormItem } from 'src/components/ui/FormItems/TextAreaFormItem';
 import { VALIDATION_CONDITION } from 'src/config/validation';
 
-import type { ValidateErrorEntity } from 'rc-field-form/lib/interface';
+import type {
+  RuleRender,
+  ValidateErrorEntity,
+} from 'rc-field-form/lib/interface';
 import s from './styles.module.scss';
 
 type AddPublicationInputs = {
@@ -16,6 +21,7 @@ type AddPublicationInputs = {
 
 export const AddPublicationForm = () => {
   const [form] = Form.useForm();
+  const [showPriceRequiredErr, setShowPriceRequiredErr] = useState(false);
 
   const onFinish = (values: AddPublicationInputs) => {
     // eslint-disable-next-line no-console
@@ -51,7 +57,7 @@ export const AddPublicationForm = () => {
     form.setFieldValue(inputName, Number(value).toFixed(2));
   };
 
-  const priceInputValidator = () => ({
+  const priceInputValidator: RuleRender = ({ getFieldValue }) => ({
     validator(_: unknown, value: string) {
       const minValue = 10;
       const maxValue = 999_999;
@@ -63,6 +69,18 @@ export const AddPublicationForm = () => {
       if (+value > maxValue) {
         return Promise.reject(new Error(`Max value is ${maxValue}`));
       }
+
+      if (
+        !getFieldValue('price-day') &&
+        !getFieldValue('price-week') &&
+        !getFieldValue('price-month')
+      ) {
+        setShowPriceRequiredErr(true);
+
+        return Promise.reject();
+      }
+
+      setShowPriceRequiredErr(false);
 
       return Promise.resolve();
     },
@@ -100,57 +118,68 @@ export const AddPublicationForm = () => {
         </Col>
       </Row>
 
-      <div className={s.priceInputsRow}>
-        <ConfigProvider
-          theme={{
-            components: {
-              InputNumber: { inputFontSize: 16, lineHeight: 1.5 },
-            },
-          }}
-        >
-          <Form.Item
-            label="грн/день"
-            name="price-day"
-            rules={[priceInputValidator]}
+      <h4 className="formItemLabel" style={{ marginBottom: 20 }}>
+        Вартість
+      </h4>
+      <div style={{ marginBottom: 40 }}>
+        <div className={s.priceInputsRow}>
+          <ConfigProvider
+            theme={{
+              components: {
+                InputNumber: { inputFontSize: 16, lineHeight: 1.5 },
+              },
+            }}
           >
-            <Input
-              type="number"
-              prefix={<HryvniaIcon />}
-              placeholder="0.00"
-              className={s.priceInput}
-              onChange={() => onChange('price-day')}
-              onBlur={() => onBlur('price-day')}
-            />
-          </Form.Item>
-          <Form.Item
-            label="грн/тиждень"
-            name="price-week"
-            rules={[priceInputValidator]}
-          >
-            <Input
-              type="number"
-              prefix={<HryvniaIcon />}
-              placeholder="0.00"
-              className={s.priceInput}
-              onChange={() => onChange('price-week')}
-              onBlur={() => onBlur('price-week')}
-            />
-          </Form.Item>
-          <Form.Item
-            label="грн/місяць"
-            name="price-month"
-            rules={[priceInputValidator]}
-          >
-            <Input
-              type="number"
-              prefix={<HryvniaIcon />}
-              placeholder="0.00"
-              className={s.priceInput}
-              onChange={() => onChange('price-month')}
-              onBlur={() => onBlur('price-month')}
-            />
-          </Form.Item>
-        </ConfigProvider>
+            <Form.Item
+              label="грн/день"
+              name="price-day"
+              rules={[priceInputValidator]}
+            >
+              <Input
+                type="number"
+                prefix={<HryvniaIcon />}
+                placeholder="0.00"
+                className={s.priceInput}
+                onChange={() => onChange('price-day')}
+                onBlur={() => onBlur('price-day')}
+              />
+            </Form.Item>
+            <Form.Item
+              label="грн/тиждень"
+              name="price-week"
+              rules={[priceInputValidator]}
+              className={s.test}
+            >
+              <Input
+                type="number"
+                prefix={<HryvniaIcon />}
+                placeholder="0.00"
+                className={s.priceInput}
+                onChange={() => onChange('price-week')}
+                onBlur={() => onBlur('price-week')}
+              />
+            </Form.Item>
+            <Form.Item
+              label="грн/місяць"
+              name="price-month"
+              rules={[priceInputValidator]}
+            >
+              <Input
+                type="number"
+                prefix={<HryvniaIcon />}
+                placeholder="0.00"
+                className={s.priceInput}
+                onChange={() => onChange('price-month')}
+                onBlur={() => onBlur('price-month')}
+              />
+            </Form.Item>
+          </ConfigProvider>
+          {showPriceRequiredErr && (
+            <p className={`${s.fieldError} ant-form-item-explain-error`}>
+              Принаймі одна ціна має бути вказана
+            </p>
+          )}
+        </div>
       </div>
       <button style={{ height: 40 }} type="submit">
         Submit
