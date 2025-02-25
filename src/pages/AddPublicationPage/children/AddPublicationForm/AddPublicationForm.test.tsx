599a44ce7e4484a39b94c8ba0ed6f9e706f34906
Alina Haisiuk <alina.gaysyuk@gmail.com>
2025-02-25 14:23:42 +0200

    Add unit tests

---
+++ src/pages/AddPublicationPage/children/AddPublicationForm/AddPublicationForm.test.tsx
@@ -0,0 +1,47 @@
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { TEXT } from 'src/config/constants';
+import { AddPublicationForm } from '.';
+
+describe('AddPublicationForm', () => {
+  it('should render the submit button', () => {
+    const { getByRole } = render(<AddPublicationForm />);
+    const submitButton = getByRole('button', { name: TEXT.PUBLISH });
+
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it('should disable submit button if form is invalid', async () => {
+    const { getByRole } = render(<AddPublicationForm />);
+    const submitButton = getByRole('button', { name: TEXT.PUBLISH });
+
+    expect(submitButton).toBeDisabled();
+  });
+
+  it('should render all form inputs', () => {
+    const { getByText } = render(<AddPublicationForm />);
+
+    const nameInput = getByText(TEXT.NAME);
+    const descriptionInput = getByText(TEXT.DESCRIPTION);
+    const priceInput = getByText(TEXT.COST);
+    const locationInput = getByText(TEXT.LOCATION_NAME);
+
+    expect(nameInput).toBeInTheDocument();
+    expect(descriptionInput).toBeInTheDocument();
+    expect(priceInput).toBeInTheDocument();
+    expect(locationInput).toBeInTheDocument();
+  });
+
+  it('should accept file upload', () => {
+    const { getByText } = render(<AddPublicationForm />);
+
+    const fileInput = getByText(TEXT.ADD_PHOTO) as HTMLInputElement;
+
+    const file = new File(['dummy content'], 'test-image.jpg', {
+      type: 'image/jpeg',
+    });
+
+    fireEvent.change(fileInput, { target: { files: [file] } });
+
+    expect(fileInput.files![0]).toBe(file);
+  });
+});
--- src/pages/AddPublicationPage/children/PriceInputs/PriceInputs.test.tsx
+++ src/pages/AddPublicationPage/children/PriceInputs/PriceInputs.test.tsx
@@ -14,20 +14,6 @@ afterEach(() => {
 });
 
 describe('PriceInputs', () => {
-  it('should throw error if inputs are empty', async () => {
-    const { getByTestId, findByText } = render(
-      <Form layout="vertical" onFinishFailed={onFinishFailed}>
-        <PriceInputs />
-        <button data-testid="submit-btn" />
-      </Form>
-    );
-
-    getByTestId('submit-btn').click();
-    await findByText(TEXT.SET_AT_LEAST_ONE_PRICE);
-
-    await waitFor(() => expect(onFinishFailed).toHaveBeenCalled());
-  });
-
   it("shouldn't throw error if at least one price set", async () => {
     const { getByTestId, getByLabelText } = render(
       <Form layout="vertical" onFinish={onFinish}>
