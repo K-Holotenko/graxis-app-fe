1fce3bcd7741e1c06a166b6dc9015ef9fb419047
OksanaLynnyk <allimpiya@gmail.com>
2025-02-25 14:47:41 +0200

    GRX-211 Add static map to the item page

--- src/pages/ItemPage/index.tsx
+++ src/pages/ItemPage/index.tsx
@@ -11,12 +11,18 @@ import { useWindowSize } from 'src/hooks/useWindowSize';
 
 import { ITEM_PAGE_CONFIG } from './utils/config';
 import { ImageCarousel } from './children/ImageCarousel';
+import { LocationMap } from './children/LocationMap';
 
 const mockedProductData = {
   title: TEXT.PRODUCT_NAME,
   category: TEXT.PRODUCT_CATEGORY,
   rating: '4,5',
   feedbackCount: 12,
+};
+
+const mockedLocationData = {
+  name: TEXT.LOCATION_NAME_MAP,
+  coordinates: { lat: 50.4501, lng: 30.5234 },
 };
 
 export const ItemPage = () => {
@@ -54,7 +60,16 @@ export const ItemPage = () => {
               </Col>
             </Row>
           }
-          bottomContent={undefined}
+          bottomContent={
+            <Row>
+              <Col span={24}>
+                <LocationMap
+                  locationName={mockedLocationData.name}
+                  coordinates={mockedLocationData.coordinates}
+                />
+              </Col>
+            </Row>
+          }
         />
       </AppLayout>
     </PageContainer>
