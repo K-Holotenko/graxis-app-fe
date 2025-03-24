b55d79c3975669d6954afe12d6634b975093fc6f
Alina Haisiuk <alina.gaysyuk@gmail.com>
2025-03-24 19:08:25 +0200

    GRX-255. Add Contact Info block

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -21,9 +21,16 @@ export interface User {
   email_verified: boolean;
 }
 
+export interface ContactInfoForm {
+  email: string;
+  phoneNumber: string;
+}
+
 interface UserStore {
   user: User | null;
   isLoading: boolean;
+  isEditingContactInfoForm: boolean;
+  contactInfoFormValues: ContactInfoForm;
   createUser: (
     user: SignUpUser,
     showError: (err: string) => void
@@ -34,11 +41,57 @@ interface UserStore {
     data: UpdateUserData,
     showError: (err: string) => void
   ) => Promise<void>;
+  setIsEditingContactInfoForm: (isEditing: boolean) => void;
+  setContactInfoFormValues: (email: string, phoneNumber: string) => void;
+  saveContactInfoFormValues: () => void;
+  isSaveButtonDisabled: boolean;
+  setIsSaveButtonDisabled: (disabled: boolean) => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
-  user: null,
+export const useUserStore = create<UserStore>((set, get) => ({
+  user: {
+    id: '123456789',
+    name: 'Vasyl',
+    surname: 'Symonenko',
+    avatar: 'https://example.com/avatar.jpg',
+    email: 'VasylSymonenko@gmail.com',
+    phoneNumber: '968756987',
+    registrationDate: '2024-03-15T10:30:00Z',
+    activeAt: '2025-03-20T14:45:00Z',
+    avatarUrl: 'https://example.com/avatar.jpg',
+    email_verified: true,
+  },
   isLoading: false,
+  isEditingContactInfoForm: false,
+  contactInfoFormValues: {
+    email: 'VasylSymonenko@gmail.com',
+    phoneNumber: '968756987',
+  },
+
+  setIsEditingContactInfoForm: (isEditing: boolean) =>
+    set({ isEditingContactInfoForm: isEditing }),
+
+  isSaveButtonDisabled: false,
+  setIsSaveButtonDisabled: (isDisabled: boolean) =>
+    set({ isSaveButtonDisabled: isDisabled }),
+
+  setContactInfoFormValues: (email: string, phoneNumber: string) =>
+    set({ contactInfoFormValues: { email, phoneNumber } }),
+
+  saveContactInfoFormValues: () => {
+    const { user, contactInfoFormValues } = get();
+
+    if (!user) return;
+    set({
+      user: {
+        ...user,
+        email: contactInfoFormValues.email,
+        phoneNumber: contactInfoFormValues.phoneNumber,
+      },
+      isEditingContactInfoForm: false,
+    });
+  },
+
   createUser: async (user, showError) => {
     set({ isLoading: true });
     const response = await signUp(user);
@@ -63,7 +116,14 @@ export const useUserStore = create<UserStore>((set) => ({
       throw new Error('Failed to fetch user');
     }
 
-    set({ user: userData, isLoading: false });
+    set({
+      user: userData,
+      isLoading: false,
+      contactInfoFormValues: {
+        email: userData.email,
+        phoneNumber: userData.phoneNumber,
+      },
+    });
   },
 
   resetUser: () => set({ user: null }),
@@ -73,7 +133,13 @@ export const useUserStore = create<UserStore>((set) => ({
       const response = await updateUser(data);
       const updatedUser: User = await response.json();
 
-      set({ user: updatedUser });
+      set({
+        user: updatedUser,
+        contactInfoFormValues: {
+          email: updatedUser.email,
+          phoneNumber: updatedUser.phoneNumber,
+        },
+      });
     } catch {
       showError('Щось пішло не так. Спробуйте ще раз');
     }
