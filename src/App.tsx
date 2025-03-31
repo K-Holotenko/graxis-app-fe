ab8e6cf320c23c82d8c5c67886a5ef6d000eff41
OksanaLynnyk <lynnyk49@gmail.com>
2025-03-31 21:57:07 +0300

    GRX-140 Fix sign up without email verification bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,47 @@
+// /// <reference types="vite-plugin-svgr/client" />
+// import { useEffect } from 'react';
+// import { RouterProvider } from 'react-router-dom';
+
+// import CookieService from 'src/services/CookieService';
+// import { useAuthStore } from 'src/stores/authStore';
+
+// import { router } from './router';
+// import { useUserStore } from './stores/userStore';
+
+// const App = () => {
+//   const { setAuthorized } = useAuthStore();
+//   const { fetchUser } = useUserStore();
+
+//   useEffect(() => {
+//     fetchUser();
+//   }, []);
+//   const { user, fetchUser } = useUserStore();
+
+//   useEffect(() => {
+//     const checkAuth = async () => {
+//       const accessToken = CookieService.getCookie('accessToken');
+
+//       if (accessToken) {
+//         try {
+//           await fetchUser();
+//           setAuthorized(!!user);
+//         } catch {
+//           setAuthorized(false);
+//           CookieService.deleteCookie('accessToken');
+//         }
+//       } else {
+//         setAuthorized(false);
+//       }
+//     };
+
+//     checkAuth();
+//   }, [setAuthorized, fetchUser]);
+
+//   return <RouterProvider router={router} />;
+// };
+
+// export default App;
+
 /// <reference types="vite-plugin-svgr/client" />
 import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
