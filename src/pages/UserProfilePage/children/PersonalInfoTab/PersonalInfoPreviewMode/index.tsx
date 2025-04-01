23f44c1c4b1ed2f7963569ec14329964078c9690
Kyrylo Holotenko <kirya.golotenko@gmail.com>
2025-04-01 23:46:13 +0300

    LET-256 [Profile] Added person info block

---
+++ src/pages/UserProfilePage/children/PersonalInfoTab/PersonalInfoPreviewMode/index.tsx
@@ -0,0 +1,31 @@
+import { Avatar } from 'antd';
+
+import { useUserStore } from 'src/stores/userStore';
+
+import styles from './styles.module.scss';
+
+export const PersonalInfoPreviewMode = () => {
+  const { user } = useUserStore();
+
+  return (
+    <div className={styles.contentContainer}>
+      <Avatar
+        size={{ xs: 100, sm: 100, md: 168, lg: 168, xl: 168, xxl: 168 }}
+        // TODO: uncomment when avatarUrl is available
+        // src={user?.avatarUrl}
+        className={styles.avatar}
+      >
+        {user?.name?.charAt(0)}
+        {user?.surname?.charAt(0)}
+      </Avatar>
+      <div className={styles.infoContainer}>
+        <div className={styles.nameContainer}>
+          <p className={styles.label}>Імʼя</p>
+          <p className={styles.value}>{user?.name || 'Не вказано'}</p>
+        </div>
+        <p className={styles.label}>Прізвище</p>
+        <p className={styles.value}>{user?.surname || 'Не вказано'}</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/UserProfilePage/children/PersonalInfoTab/index.tsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from 'react';
+import { FormInstance } from 'antd';
+
+import { useUserStore } from 'src/stores/userStore';
+
+import { PersonalInfo } from './PersonalInfo';
+import { ContactInfo } from './ContactInfo';
+import styles from './styles.module.scss';
+
+interface PersonalInfoTabProps {
+  form: FormInstance;
+  shouldShowDialog: boolean;
+  isPersonalInfoEditModeEnabled: boolean;
+  isContactInfoEditModeEnabled: boolean;
+  isValid: boolean;
+  handleShowDialog: (shouldShow: boolean) => void;
+  onPersonalInfoEditClick: (isEdit: boolean) => void;
+  onContactInfoEditClick: (isEdit: boolean) => void;
+  onPersonalInfoValidation: (valid: boolean) => void;
+  onContactInfoValidation: (valid: boolean) => void;
+}
+
+export const PersonalInfoTab = ({
+  form,
+  shouldShowDialog,
+  isPersonalInfoEditModeEnabled,
+  isContactInfoEditModeEnabled,
+  isValid,
+  handleShowDialog,
+  onPersonalInfoEditClick,
+  onContactInfoEditClick,
+  onPersonalInfoValidation,
+  onContactInfoValidation,
+}: PersonalInfoTabProps) => {
+  const { user } = useUserStore();
+  const [hasContactInfoChanged, setHasContactInfoChanged] = useState(false);
+  const [hasPersonalInfoChanged, setHasPersonalInfoChanged] = useState(false);
+
+  const handleFieldChange = () => {
+    if (!form.isFieldsTouched() || !user) return;
+
+    const values = form.getFieldsValue();
+
+    // TODO Add avatarUrl when the BE is updated
+    const personalInfoFields: Array<keyof typeof user> = ['name', 'surname'];
+    const contactInfoFields: Array<keyof typeof user> = [
+      'email',
+      'phoneNumber',
+    ];
+
+    const personalInfoHasChanges = personalInfoFields.some(
+      (field) => form.isFieldTouched(field) && values[field] !== user[field]
+    );
+
+    const contactInfoHasChanges = contactInfoFields.some(
+      (field) => form.isFieldTouched(field) && values[field] !== user[field]
+    );
+
+    setHasContactInfoChanged(contactInfoHasChanges);
+    setHasPersonalInfoChanged(personalInfoHasChanges);
+    const ifSomeFieldChanged = personalInfoHasChanges || contactInfoHasChanges;
+
+    handleShowDialog(ifSomeFieldChanged);
+  };
+
+  // Handles dialog visibility when the user clicks on the edit button
+  useEffect(() => {
+    const isPersonalInfoChangedAndEditEnabled =
+      hasPersonalInfoChanged && isPersonalInfoEditModeEnabled;
+    const isContactInfoChangedAndEditEnabled =
+      hasContactInfoChanged && isContactInfoEditModeEnabled;
+
+    handleShowDialog(
+      isContactInfoChangedAndEditEnabled || isPersonalInfoChangedAndEditEnabled
+    );
+  }, [isPersonalInfoEditModeEnabled, isContactInfoEditModeEnabled]);
+
+  useEffect(() => {
+    if (!isValid || !shouldShowDialog) {
+      return;
+    }
+
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [shouldShowDialog]);
+
+  return (
+    <section>
+      <div className={styles.firstCardWrapper}>
+        <PersonalInfo
+          form={form}
+          onChange={handleFieldChange}
+          onPersonalInfoValidation={onPersonalInfoValidation}
+          onPersonalInfoEditClick={onPersonalInfoEditClick}
+          isPersonalInfoEditModeEnabled={isPersonalInfoEditModeEnabled}
+        />
+      </div>
+      <ContactInfo
+        form={form}
+        onChange={handleFieldChange}
+        onContactInfoValidation={onContactInfoValidation}
+        onContactInfoEditClick={onContactInfoEditClick}
+        isContactInfoEditModeEnabled={isContactInfoEditModeEnabled}
+      />
+    </section>
+  );
+};
