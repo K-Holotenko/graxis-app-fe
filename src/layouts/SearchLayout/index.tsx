0dbaa6eac5b8c92cca3d51e387b6348cda5ea931
OksanaLynnyk <allimpiya@gmail.com>
2025-04-11 19:46:59 +0300

    GRX-316 [Search Page] Scroll to top. Fix comments

--- src/layouts/SearchLayout/index.tsx
+++ src/layouts/SearchLayout/index.tsx
@@ -1,5 +1,8 @@
-import { Col, Row } from 'antd';
+import { Col, Row, FloatButton, ConfigProvider } from 'antd';
 import { ReactNode } from 'react';
+
+import { theme } from 'src/config/theme';
+import ArrowUpIcon from 'src/assets/icons/arrow-up.svg?react';
 
 import styles from './styles.module.scss';
 
@@ -30,5 +33,22 @@ export const SearchLayout = ({
         {loadMoreButton}
       </Col>
     </Row>
+    <ConfigProvider theme={localTheme}>
+      <FloatButton.BackTop className={styles.floatBtn} icon={<ArrowUpIcon />} />
+    </ConfigProvider>
   </>
 );
+
+const localTheme = {
+  token: {
+    controlHeightLG: 48,
+    margin: 0,
+    fontSizeIcon: 24,
+    colorFillContent: theme.N2,
+    boxShadowSecondary: `
+          0 6px 8px 0 rgba(80, 86, 94, 0.12),
+          0 3px 25px 0 rgba(80, 86, 94, 0.12),
+          0 9px 14px 0 rgba(80, 86, 94, 0.02)
+        `,
+  },
+};
