b0b3cf6255d8cc608cd874d3b3c15baf36702a74
OksanaLynnyk <allimpiya@gmail.com>
2025-04-15 12:16:45 +0300

    GRX-323 [My Publications] Empty state. Fix comments

--- src/pages/MyPublicationsPage/children/MyPublicationsEmptyState/index.tsx
+++ src/pages/MyPublicationsPage/children/MyPublicationsEmptyState/index.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
 import { Button } from 'src/components/Button';
-import NoMypblications from 'src/assets/icons/no-my-publications.svg?react';
+import NoMyPublicationsImg from 'src/assets/icons/no-my-publications.svg?react';
 import PlusIcon from 'src/assets/icons/plus-icon.svg?react';
 
 import styles from './styles.module.scss';
@@ -9,28 +9,28 @@ import styles from './styles.module.scss';
 interface MyPublicationsEmptyStateProps {
   message: string;
   buttonLabel: string;
-  navigateTo: string;
+  route: string;
   showIcon?: boolean;
 }
 
 export const MyPublicationsEmptyState = ({
   message,
   buttonLabel,
-  navigateTo,
+  route,
   showIcon = false,
 }: MyPublicationsEmptyStateProps) => {
   const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
-      <NoMypblications className={styles.noMyPublicationsImg} />
+      <NoMyPublicationsImg className={styles.noMyPublicationsImg} />
       <p className={styles.message}>{message}</p>
       <Button
         className={styles.button}
         label={buttonLabel}
         icon={showIcon ? <PlusIcon /> : undefined}
         iconPosition="end"
-        onClick={() => navigate(navigateTo)}
+        onClick={() => navigate(route)}
       />
     </div>
   );
