042a3282ca3ae539a2338dcac77cedf303ed518a
OksanaLynnyk <allimpiya@gmail.com>
2025-04-17 09:10:40 +0000

    GRX-318 [Publication page] Breadcrumbs sync with BE

--- src/components/BreadCrumbs/utils/generateBreadcrumbs.ts
+++ src/components/BreadCrumbs/utils/generateBreadcrumbs.ts
@@ -1,10 +1,16 @@
 import { TEXT } from 'src/config/constants';
+import {
+  findCategoryByValue,
+  findPathByValue,
+  getAllDescendantsValues,
+} from 'src/pages/SearchPage/children/Filters/utils/filters';
+import { ROUTES } from 'src/router/routes';
 
-import { BreadcrumbItem, buildBreadcrumbs } from './buildBreadcrumbs';
+import { BreadcrumbItem, buildBreadcrumbsPath } from './buildBreadcrumbsPath';
 
 interface GenerateBreadcrumbsProps {
-  items: BreadcrumbItem[];
-  currentItem: string;
+  breadcrumbs: BreadcrumbItem[];
+  currentBreadcrumb: string;
   showAllItems?: boolean;
 }
 
@@ -16,22 +22,41 @@ export interface GeneratedBreadcrumbItem {
 }
 
 export const generateBreadcrumbs = ({
-  items,
-  currentItem,
+  breadcrumbs,
+  currentBreadcrumb,
   showAllItems = false,
 }: GenerateBreadcrumbsProps): GeneratedBreadcrumbItem[] | null => {
-  const path = buildBreadcrumbs(items, currentItem);
+  const path = findPathByValue(breadcrumbs, currentBreadcrumb);
 
-  if (!path) return null;
+  if (!path.length) return null;
+
+  const pathItems = buildBreadcrumbsPath(breadcrumbs, path);
 
   const fullPath = showAllItems
-    ? [{ value: 'all', title: TEXT.ALL_CATEGORIES }, ...path]
-    : path;
+    ? [{ value: 'all', title: TEXT.ALL_CATEGORIES, children: [] }, ...pathItems]
+    : pathItems;
 
-  return fullPath.map(({ value, title }, index) => ({
-    key: value,
-    title,
-    url: '/',
-    isLast: index === fullPath.length - 1,
-  }));
+  return fullPath.map(({ value, title }, index) => {
+    const isLast = index === fullPath.length - 1;
+    const base = ROUTES.SEARCH_RESULTS;
+
+    const categories =
+      value === 'all'
+        ? []
+        : isLast
+          ? [value]
+          : getAllDescendantsValues(findCategoryByValue(breadcrumbs, value)!);
+
+    const url =
+      categories.length > 0
+        ? `${base}?categories=${categories.join('%2C')}`
+        : base;
+
+    return {
+      key: value,
+      title,
+      url,
+      isLast,
+    };
+  });
 };
