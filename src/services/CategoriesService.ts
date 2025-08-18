603d1272796b8ede19416482da3001407dcc9be0
Kyrylo Holotenko <kirya.golotenko@gmail.com>
2025-08-18 21:32:45 +0300

    NONE Update backend endpoint

--- src/services/CategoriesService.ts
+++ src/services/CategoriesService.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-import { GRAXIS_API_URL } from 'src/config/constants';
 import { Category } from 'src/stores/categoriesStore';
 
 export const getAllCategories = async (): Promise<Category[]> => {
-  const response = await axios.get(`${GRAXIS_API_URL}/categories`);
+  const response = await axios.get(
+    `${import.meta.env.VITE_APP_GRAXIS_API_URL}/categories`
+  );
 
   return response.data;
 };
