3e351b2260545c95a015e55c88e77f22d4541b6a
OksanaLynnyk <allimpiya@gmail.com>
2025-09-14 20:34:21 +0000

    GRX-433 [My Publications] Empty state and sceleton loader check

--- src/pages/MyPublicationsPage/index.tsx
+++ src/pages/MyPublicationsPage/index.tsx
@@ -1,17 +1,30 @@
+import { useParams } from 'react-router-dom';
+
 import { PageContainer } from 'src/layouts/PageContainer';
 import { MyPublicationsLayout } from 'src/layouts/MyPublicationsLayout';
 import { AppLayout } from 'src/layouts/AppLayout';
 import { Heading } from 'src/components/Heading';
+import { PublicationFilters } from 'src/stores/myPublicationStore';
+import { NotFoundPage } from 'src/pages/NotFoundPage';
 
 import { MyTabs } from './children/Tabs';
 
-export const MyPublicationsPage = () => (
-  <PageContainer pageTitle="Мої оголошення">
-    <AppLayout>
-      <MyPublicationsLayout
-        title={<Heading level={2}>Мої оголошення</Heading>}
-        tabs={<MyTabs />}
-      />
-    </AppLayout>
-  </PageContainer>
-);
+export const MyPublicationsPage = () => {
+  const { tab } = useParams<{ tab: PublicationFilters }>();
+  const validTabs = Object.values(PublicationFilters);
+
+  if (!tab || !validTabs.includes(tab as PublicationFilters)) {
+    return <NotFoundPage />;
+  }
+
+  return (
+    <PageContainer pageTitle="Мої оголошення">
+      <AppLayout>
+        <MyPublicationsLayout
+          title={<Heading level={2}>Мої оголошення</Heading>}
+          tabs={<MyTabs />}
+        />
+      </AppLayout>
+    </PageContainer>
+  );
+};
